d0479dd8b78a023f03b29f9d035b9896f639c180
RahealMiya <rahealmiya328@gmail.com>
2022-07-08 11:38:38 +0530

    added

--- src/app/viewstudent/viewstudent.component.ts
+++ src/app/viewstudent/viewstudent.component.ts
@@ -11,6 +11,13 @@ export class ViewstudentComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  name="Miya"
+  data=[{"name":"Rahul","age":23,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8__Nzyv7MNSIA71YRqKt0rPoqEiSiZMZBOw&usqp=CAU"},
+  {"name":"Miya","age":20,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8__Nzyv7MNSIA71YRqKt0rPoqEiSiZMZBOw&usqp=CAU"},
+  {"name":"Akshaya","age":20,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8__Nzyv7MNSIA71YRqKt0rPoqEiSiZMZBOw&usqp=CAU"},
+  {"name":"Sona","age":20,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8__Nzyv7MNSIA71YRqKt0rPoqEiSiZMZBOw&usqp=CAU"},
+  {"name":"Aadhi","age":20,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8__Nzyv7MNSIA71YRqKt0rPoqEiSiZMZBOw&usqp=CAU"},
+  {"name":"Anisha","age":20,"photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8__Nzyv7MNSIA71YRqKt0rPoqEiSiZMZBOw&usqp=CAU"}
+
+  ]
 
 }
